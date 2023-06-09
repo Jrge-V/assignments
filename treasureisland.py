73706ae6269afbb5dced7dcb685eec53307328a0
Jrge-V <jorgeverduzco91@gmail.com>
2023-06-09 14:29:44 -0700

    first assignment

---
+++ treasureisland.py
@@ -0,0 +1,157 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+print("\nThere's a Jungle to your right and Sand path to your left.")
+move = (input("Where would you like to go? Left or Right?"))
+
+move = move.lower()
+
+if move == "right":
+    print('''
+                .        +          .      .          .
+     .            _        .                    .
+  ,              /;-._,-.____        ,-----.__
+ ((        .    (_:#::_.:::. `-._   /:, /-._, `._,
+  `                 \   _|`"=:_::.`.);  \ __/ /
+                      ,    `./  \:. `.   )==-'  .
+    .      ., ,-=-.  ,\, +#./`   \:.  / /           .
+.           \/:/`-' , ,\ '` ` `   ): , /_  -o
+       .    /:+- - + +- : :- + + -:'  /(o-) \)     .
+  .      ,=':  \    ` `/` ' , , ,:' `'--".--"---._/`7
+   `.   (    \: \,-._` ` + '\, ,"   _,--._,---":.__/
+              \:  `  X` _| _,\/'   .-'
+.               ":._:`\____  /:'  /      .           .
+                    \::.  :\/:'  /              +
+   .                 `.:.  /:'  }      .
+           .           ):_(:;   \           .
+                      /:. _/ ,  |
+                   . (|::.     ,`                  .
+     .                |::.    {\
+                      |::.\  \ `.
+                      |:::(\    |
+              O       |:::/{ }  |                  (o
+               )  ___/#\::`/ (O "==._____   O, (O  /`
+          ~~~w/w~"~~,\` `:/,-(~`"~~~~~~~~"~o~\~/~w|/~
+dew   ~~~~~~~~~~~~~~~~~~~~~~~\\W~~~~~~~~~~~~\|/~~
+    ''')
+
+    print("You discovered that the jungle is like a maze, you get lost with no way out. Game Over.")
+elif move == "left":
+    print("You continue walking and hear strange noises coming from the jungle behind you."
+          " \nThe sun has set and it is nightfall, you have now approached the end of the sand path, with only water in front of you.")
+    move = (input("Would you like to swim or wait till sunset? Swim or Wait?")).lower()
+    if move == "swim":
+        print('''
+                                ___
+                     .-'   `'.
+                    /         \
+                    |         ;
+                    |         |           ___.--,
+           _.._     |0) ~ (0) |    _.---'`__.-( (_.
+    __.--'`_.. '.__.\    '--. \_.-' ,.--'`     `""`
+   ( ,.--'`   ',__ /./;   ;, '.__.'`    __
+   _`) )  .---.__.' / |   |\   \__..--""  """--.,_
+  `---' .'.''-._.-'`_./  /\ '.  \ _.-~~~````~~~-._`-.__.'
+        | |  .' _.-' |  |  \  \  '.               `~---`
+         \ \/ .'     \  \   '. '-._)
+          \/ /        \  \    `=.__`~-.
+     jgs  / /\         `) )    / / `"".`\
+    , _.-'.'\ \        / /    ( (     / /
+     `--~`   ) )    .-'.'      '.'.  | (
+            (/`    ( (`          ) )  '-;
+             `      '-;         (-'
+             ''')
+        print("You decided to swim in the darkness. You get pulled from underneath and are unable to swim back up. Game Over.")
+    elif move == "wait":
+        print("You wait till the sun rises and notice that the waters are infested with giant octupus.")
+        print("At the distance you can now see a smaller island, and to your left you discover there was a boat the entire time.")
+        print("You take the boat and sail over to the smaller island, the octopus seem to be ignoring you.")
+        print("You've arrived at the smaller island and are greeted with three doors.")
+        move = (input("The doors are Red, Blue , and Yellow. Which do you take?")).lower()
+
+        if move == "red":
+            print('''
+                           (  .      )
+           )           (              )
+                 .  '   .   '  .  '  .
+        (    , )       (.   )  (   ',    )
+         .' ) ( . )    ,  ( ,     )   ( .
+      ). , ( .   (  ) ( , ')  .' (  ,    )
+     (_,) . ), ) _) _,')  (, ) '. )  ,. (' )
+ jgs^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+            ''')
+            print("You go inside the red door and are engulfed by flames. Game Over.")
+        elif move == "blue":
+            print('''
+                                        ,--.
+                           {    }
+                           K,   }
+                          /  ~Y`
+                     ,   /   /
+                    {_'-K.__/
+                      `/-.__L._
+                      /  ' /`\_}
+                     /  ' /
+             ____   /  ' /
+      ,-'~~~~    ~~/  ' /_
+    ,'             ``~~~  ',
+   (                        Y
+  {                         I
+ {      -                    `,
+ |       ',                   )
+ |        |   ,..__      __. Y
+ |    .,_./  Y ' / ^Y   J   )|
+ \           |' /   |   |   ||
+  \          L_/    . _ (_,.'(
+   \,   ,      ^^""' / |      )
+     \_  \          /,L]     /
+       '-_~-,       ` `   ./`
+          `'{_            )
+              ^^\..___,.--`
+            ''')
+
+            print("You go inside the blue door and become trapped in a labyrinth. Game Over.")
+        elif move == "yellow":
+            print('''
+                                             _       
+                                | |      
+  ___ ___  _ __   __ _ _ __ __ _| |_ ___ 
+ / __/ _ \| '_ \ / _` | '__/ _` | __/ __|
+| (_| (_) | | | | (_| | | | (_| | |_\__ \
+ \___\___/|_| |_|\__, |_|  \__,_|\__|___/
+                  __/ |                  
+                 |___/                 
+            ''')
+            print("You go inside the yellow door and see a treasure in the distance. You open it and see golden coins! Congrats, You Win!")
+        else:
+            print("You are indecisive, Game Over!")
+    else:
+        print("You are indecisive, Game Over!")
+else:
+    print("You are indecisive, Game Over!")
